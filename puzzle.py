6bcd1ded3c8db173d6157fdf4c55d854fd03098e
Olha Liuba <olgaluba3@gmail.com>
2021-02-05 19:02:59 +0200

    Add puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,7 @@
+"""
+This module is created to check whether the board is ready for the game.
+It mustn't contain same numbers in one row, in one column and on one color.
+
+Github link: https://github.com/oliuba/puzzle_row_column_color
+"""
+
