cbebb8e1ead95a9d216d674c45a0b9973f72a5ea
Olha Liuba <olgaluba3@gmail.com>
2021-02-05 19:36:19 +0200

    Add row_valid function

--- puzzle.py
+++ puzzle.py
@@ -5,3 +5,30 @@ It mustn't contain same numbers in one row, in one column and on one color.
 Github link: https://github.com/oliuba/puzzle_row_column_color
 """
 
+def row_valid(board: list) -> bool:
+    """
+    Returns True if the board is ready in rows (each row has unrepeatable numbers).
+    >>> board = ["**** **",\
+    "***1 **",\
+    "**  3**",\
+    "* 4 1 9",\
+    "     8 ",\
+    " 61  35",\
+    "3  8   ",\
+    "      *",\
+    "**2 5**",\
+    "** 2***",\
+    "** ****"]
+    >>> row_valid(board)
+    True
+    """
+    for row in board:
+        playing_zone = row.replace('*', '')
+        only_numbers = playing_zone.replace(' ', '')
+        if len(only_numbers) != len(set(only_numbers)):
+            return False
+    return True
+
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod()
